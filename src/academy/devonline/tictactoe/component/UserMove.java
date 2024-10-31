9923dffab9425161363a2564fecca5e400da8de8
Polina-creator05 <polinager502@gmail.com>
2024-10-31 13:16:58 +0300

    Implement UserMove component

--- src/academy/devonline/tictactoe/component/UserMove.java
+++ src/academy/devonline/tictactoe/component/UserMove.java
@@ -17,7 +17,10 @@
 
 package academy.devonline.tictactoe.component;
 
+import academy.devonline.tictactoe.model.Cell;
 import academy.devonline.tictactoe.model.GameTable;
+
+import java.util.Scanner;
 
 /**
  * @author devonline
@@ -25,8 +28,44 @@ import academy.devonline.tictactoe.model.GameTable;
  */
 public class UserMove {
 
+    final char[][] mapping = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'}
+    };
+
 
     public void make(final GameTable gameTable) {
+        while (true) {
+            final Cell cell = getUserInput();
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, 'x');
+                return;
+            } else {
+                System.out.println("Can't make a move, because the cell is not free! Try again!");
+            }
+        }
+    }
 
+    private Cell getUserInput() {
+        while (true) {
+            System.out.println("Please type number between 1 and 9: ");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                final char ch = userInput.charAt(0);
+                if (ch > '0' && ch <= '9') {
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if (mapping[i][j] == ch) {
+                                return new Cell(i, j);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
