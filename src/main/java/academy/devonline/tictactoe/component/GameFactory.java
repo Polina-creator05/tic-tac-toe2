058ce06bf440403f474d9c2b78fd4612fb85c967
Polina-creator05 <polinager502@gmail.com>
2024-12-05 23:06:24 +0300

    Implement GameFactory logic

--- src/main/java/academy/devonline/tictactoe/component/GameFactory.java
+++ src/main/java/academy/devonline/tictactoe/component/GameFactory.java
@@ -22,27 +22,65 @@ import academy.devonline.tictactoe.component.keypad.TerminalNumericKeypadCellNum
 import academy.devonline.tictactoe.model.Player;
 import academy.devonline.tictactoe.model.PlayerType;
 
+import static academy.devonline.tictactoe.model.PlayerType.COMPUTER;
+import static academy.devonline.tictactoe.model.PlayerType.USER;
 import static academy.devonline.tictactoe.model.Sign.O;
 import static academy.devonline.tictactoe.model.Sign.X;
 
 public class GameFactory {
 
-    private PlayerType playerType1 = PlayerType.USER;
+    private final PlayerType playerType1;
 
-    private PlayerType playerType2 = PlayerType.COMPUTER;
+    private final PlayerType playerType2;
 
     public GameFactory(final String[] args) {
-        //TODO
+        PlayerType playerType1 = null;
+        PlayerType playerType2 = null;
+        for (final String arg : args) {
+            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
+                if (playerType1 == null) {
+                    playerType1 = PlayerType.valueOf(arg.toUpperCase());
+                } else if (playerType2 == null) {
+                    playerType2 = PlayerType.valueOf(arg.toUpperCase());
+                }
+            } else {
+                System.err.println("Unsupported command line argument: '" + arg + "'");
+            }
+        }
+        if (playerType1 == null) {
+            this.playerType1 = USER;
+            this.playerType2 = COMPUTER;
+        } else if (playerType2 == null) {
+            this.playerType1 = USER;
+            this.playerType2 = playerType1;
+        } else {
+            this.playerType1 = playerType1;
+            this.playerType2 = playerType2;
+        }
+
     }
 
     public Game create() {
         final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
+        final Player player1;
+        if (playerType1 == USER) {
+            player1 = new Player(X, new UserMove(cellNumberConverter));
+        } else {
+            player1 = new Player(X, new ComputerMove());
+        }
+        final Player player2;
+        if (playerType2 == USER) {
+            player2 = new Player(O, new UserMove(cellNumberConverter));
+        } else {
+            player2 = new Player(O, new ComputerMove());
+        }
+
+        final boolean canSecondPlayerMakeFirstMove = playerType1 != playerType2;
         return new Game(new DataPrinter(cellNumberConverter),
-                //FIXME
-                new Player(X, new UserMove(cellNumberConverter)),
-                new Player(O, new ComputerMove()),
+                player1,
+                player2,
                 new WinnerVerifier(),
                 new CellVerifier(),
-                true);
+                canSecondPlayerMakeFirstMove);
     }
 }
--- src/main/java/academy/devonline/tictactoe/model/PlayerType.java
+++ src/main/java/academy/devonline/tictactoe/model/PlayerType.java
@@ -22,4 +22,5 @@ public enum PlayerType {
     USER,
 
     COMPUTER
+
 }
