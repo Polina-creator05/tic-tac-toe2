9aa68d6d9e9a9bb8a6402e8cca9ef794aca87014
Polina-creator05 <polinager502@gmail.com>
2024-12-18 16:00:51 +0300

    Add FirstMoveToTheCenterComputerMoveStrategy

--- src/main/java/academy/devonline/tictactoe/GameFactory.java
+++ src/main/java/academy/devonline/tictactoe/GameFactory.java
@@ -24,6 +24,7 @@ import academy.devonline.tictactoe.component.console.ConsoleDatePrinter;
 import academy.devonline.tictactoe.component.console.ConsoleGameOverHandler;
 import academy.devonline.tictactoe.component.console.ConsoleUserInputReader;
 import academy.devonline.tictactoe.component.console.keypad.DesktopNumericKeypadCellNumberConverter;
+import academy.devonline.tictactoe.component.strategies.FirstMoveToTheCenterComputerMoveStrategy;
 import academy.devonline.tictactoe.component.strategies.RandomComputerMoveStrategy;
 import academy.devonline.tictactoe.component.swing.GameWindow;
 import academy.devonline.tictactoe.model.config.PlayerType;
@@ -52,6 +53,7 @@ public class GameFactory {
 
     public Game create() {
         final ComputerMoveStrategy[] strategies={
+                new FirstMoveToTheCenterComputerMoveStrategy(),
                 new RandomComputerMoveStrategy()
 
         };
